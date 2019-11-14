307c31cd2cabd5d34772637e97601f733c0b3341
sabu.shakya <sabu.shakya@f1soft.com>
2019-11-14 20:00:07 +0545

    Add client and server modules

---
+++ server/users.js
@@ -0,0 +1,24 @@
+const users = [];
+
+const addUser = ({id,name,room})=>{
+    name= name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find(user= user.room === room) && user.name === name;
+    if (existingUser){
+        return {error: 'User is taken.'};
+    }
+
+    const user ={id,name,room};
+    users.push(user);
+
+};
+const removeUser = (id)=>{
+
+};
+const getUser = ()=>{
+
+};
+const getUsersInRoom = ()=>{
+
+};
