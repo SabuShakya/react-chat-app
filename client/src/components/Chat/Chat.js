7dc0c4f362b8b97aaa66be27e7869b42d130d13c
sabu.shakya <sabu.shakya@f1soft.com>
2019-11-14 22:26:34 +0545

    Add user helper(crud) methods and add event listeners for join and sendMessage

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -11,21 +11,20 @@ const Chat = ({location}) => {
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
-
-        socket = io(ENDPOINT);
-
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room},()=>{
-        });
+        socket = io(ENDPOINT);
+
+        socket.emit('join', {name, room},()=>{});
 
         return ()=>{
             socket.emit('disconnect');
             socket.off();
         }
-
     }, [ENDPOINT, location.search]);
+
+
     return (
         <h1>Chat</h1>
     );
