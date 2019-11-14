5d7745c8d900e09682023d756652395f978e642a
sabu.shakya <sabu.shakya@f1soft.com>
2019-11-14 22:28:31 +0545

    Add user helper(crud) methods and add event listeners for join and sendMessage

--- server/users.js
+++ server/users.js
@@ -1,24 +1,28 @@
 const users = [];
 
-const addUser = ({id,name,room})=>{
-    name= name.trim().toLowerCase();
+const addUser = ({id, name, room}) => {
+    name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
-    const existingUser = users.find(user= user.room === room) && user.name === name;
-    if (existingUser){
-        return {error: 'User is taken.'};
+    const existingUser = users.find(user => user.room === room && user.name === name);
+    if (existingUser) {
+        return {error: 'Username is taken.'};
     }
 
-    const user ={id,name,room};
-    users.push(user);
-
+    const user = {id, name, room};
+    users.push(users);
+    return {user};
 };
-const removeUser = (id)=>{
 
+const removeUser = id => {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
 };
-const getUser = ()=>{
 
-};
-const getUsersInRoom = ()=>{
+const getUser = id => user.find(user => user.id === id);
 
-};
+const getUsersInRoom = room => users.filter(user => user.room === room);
+
+module.exports = {addUser,getUser,removeUser,getUsersInRoom};
